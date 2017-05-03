a72df3ecaee2d7e9401a5c233ab20ca70b2ca5a5
Gustav Bergman <gubergma@gmail.com>
2017-05-03 14:23:23 +0200

    added page navigator function

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	// Track the current page, starts with page 1
+	var currentpage = '#page1';
+
+	$('.navBtn').click(function(){
+		var targetpage = $('#'+$(this).attr('targetpage'));
+		show(targetpage);
+	});
+
+	var show = function(targetpage){
+		$(currentpage).hide()
+		targetpage.show();
+		currentpage = targetpage;
+	}
+});
+
