b101aa46146deb273d382ecae765187c2b9c3a59
Erik Lindström <erik-lindstrom94@hotmail.com>
2017-05-05 14:57:29 +0200

    merging

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	// Track the current page, starts with page 1
+	var currentpage = '#page1';
+
+	$('.navBtn').click(function(){
+		var targetpage = $('#'+$(this).attr('targetpage'));
+		show(targetpage);
+	});
+
+	var show = function(targetpage){
+		$(currentpage).hide()
+		targetpage.show();
+		currentpage = targetpage;
+	}
+});
