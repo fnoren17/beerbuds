48851952159c0262589456fa33b7145ab0b774f9
fnoren17 <fnoren17@gmail.com>
2017-05-05 15:54:29 +0200

    Help dialog

--- js/main.js
+++ js/main.js
@@ -14,3 +14,15 @@ $(document).ready(function() {
 		currentpage = targetpage;
 	}
 });
+
+var dialog = document.querySelector('dialog');
+var showDialogButton = document.querySelector('#show-dialog');
+if (! dialog.showModal) {
+  dialogPolyfill.registerDialog(dialog);
+}
+showDialogButton.addEventListener('click', function() {
+  dialog.showModal();
+});
+dialog.querySelector('.close').addEventListener('click', function() {
+  dialog.close();
+});
