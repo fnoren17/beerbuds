2694acdf429cc473751c397a8d34d99391df6106
antmar <antmar@kth.se>
2017-05-12 14:20:58 +0200

    Småsaker för newsfeed och isActive

--- js/newsfeed.js
+++ js/newsfeed.js
@@ -1,9 +1,9 @@
 function populateFeed() {
 
 firebase.database().ref('/users/').on('value', function(snapshot) {
-			snapshot.forEach(function(childSnapshot) {
-				console.log(childSnapshot);
-				});
+	snapshot.forEach(function(childSnapshot) {
+		console.log(childSnapshot.A.k.ba.left);
+		});
 
 });
 }
