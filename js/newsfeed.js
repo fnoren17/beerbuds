4a66ac70e9e1dc591813ae5d906facb69c8ac0c5
antmar <antmar@kth.se>
2017-05-15 10:03:23 +0200

    Ändra namn i settings

--- js/newsfeed.js
+++ js/newsfeed.js
@@ -5,32 +5,27 @@ ref.orderByChild("isActive").equalTo(true).on("child_added", function(snapshot) 
 	var activeUsers = [];
 	snapshot.forEach(testfunc(snapshot));
 
-	});//console.log("snapshot.child(name).val() ", snapshot.child("name").val()));//function(){
-		//var firebaseName = document.getElementById("nameOfUsers");
-		//console.log("snapshot.child(name).val() ", snapshot.child("name").val());
-		//firebaseName.innerText = snapshot.child("name").val();
-		//console.log("names", firebaseName.innerText)
-
-	//});//console.log(snapshot.child("name").val()));
-
-		//console.log(snapshot);
-		//var firebaseNameRef = firebase.database().ref('users/' + userID).child("name")
-		//console.log(firebaseNameRef);
-	//});
-//});
+	});
 
 }
 
 function testfunc(snapshot) {
+	console.log(snapshot.child('position').val());
+	var position = snapshot.child('position').val();
 	var firebaseName = document.getElementById("activeUsers");
-	firebaseName.innerHTML += '<div class="row" id="flex" style="border: 1px solid black; border-radius: 5px;"><div class="col-4"><img src="https://scontent-arn2-1.xx.fbcdn.net/v/t31.0-8/15128952_10207573719443039_6551904099240725029_o.jpg?oh=3fb5df0585953f3bb5f5b8b42d67460a&oe=59B936A6" id="profilepic" width="100px" height="100px"></img></div><div class="col-8" style="font-size: 10pt"><b>' + snapshot.child("name").val() + '</b> wants to get his drink on <i><br/>near Östermalmstorg</i><br/><br/>Swipe right to start chatting ></div></div>'
+	firebaseName.innerHTML += '<div onclick="showPos(' + position + ')" class="row" targetpage="page3" id="flex" style="border: 1px solid black; border-radius: 5px;"><div class="col-4"><img src="https://scontent-arn2-1.xx.fbcdn.net/v/t31.0-8/15128952_10207573719443039_6551904099240725029_o.jpg?oh=3fb5df0585953f3bb5f5b8b42d67460a&oe=59B936A6" id="profilepic" width="100px" height="100px"></img></div><div class="col-8" style="font-size: 10pt"><b>' + snapshot.child("name").val() + '</b> wants to get his drink on <i><br/>near Östermalmstorg</i><br/><br/>Swipe right to start chatting ></div></div>'
 
+}
 
-  // var userID = auth.currentUser.uid;
-  // var firebaseNameRef = firebase.database().ref('users/' + userID).child("name")
-  // console.log("Ref: ",firebaseNameRef);
-  // firebaseNameRef.on("value", function(datasnapshot) {
-  //   Name.innerText = datasnapshot.val();
-  //   console.log("Name: ", Name.innerText);
-  //  });
+function showPos(position) {
+	currentpage = "#page2"
+		$('.row').click(function(){
+		var targetpage = $('#'+$(this).attr('targetpage'));
+		show(targetpage);
+	});
+		var show = function(targetpage){
+		$(currentpage).hide()
+		targetpage.show();
+		currentpage = targetpage;
+	}
 }
