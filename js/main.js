055730e068fe8325f9c1dfb52216fd717cd7e736
Gustav Bergman <gubergma@gmail.com>
2017-05-16 08:22:44 +0200

    fixing map n shit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 	// Track the current page, starts with page 1
 	var currentpage = '#page1';
 
 	$('.navBtn').click(function(){
 		var targetpage = $('#'+$(this).attr('targetpage'));
-		show(targetpage);
+		showpage(targetpage);
 	});
 
-	var show = function(targetpage){
+	var showpage = function(targetpage){
 		$(currentpage).hide()
 		targetpage.show();
 		currentpage = targetpage;
 	}
-});
+// });
